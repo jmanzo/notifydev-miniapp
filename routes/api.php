9f57c540d65b8d436857457418a0d569195d4476
Jean Manzo <jmanzosistemas@gmail.com>
2017-07-06 01:59:26 -0400

    created models

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,10 @@ use Illuminate\Http\Request;
 |
 */
 
+Route::post( '/', function (Request $request) {
+	return response()->json($request->all());
+} );
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
