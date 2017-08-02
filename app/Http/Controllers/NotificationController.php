d7cc9d35c4c71f1fdf090a90ef06463c64a21581
Jean Manzo <jmanzosistemas@gmail.com>
2017-08-02 17:04:13 -0400

    [TNP-6] Receive and process multiple Sendgrid events

---
+++ app/Http/Controllers/NotificationController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use NotificationChannels\OneSignal\OneSignalChannel;
+use NotificationChannels\OneSignal\OneSignalMessage;
+use NotificationChannels\OneSignal\OneSignalWebButton;
+use Illuminate\Notifications\Notification;
+
+class NotificationController extends Controller
+{
+    public function via($notifiable)
+    {
+        return [OneSignalChannel::class];
+    }
+
+    public function toOneSignal($notifiable)
+    {
+        return OneSignalMessage::create()
+            ->subject("Your {$notifiable->service} account was approved!")
+            ->body("Click here to see details.")
+            ->url('http://onesignal.com')
+            ->webButton(
+                OneSignalWebButton::create('link-1')
+                    ->text('Click here')
+                    ->icon('https://upload.wikimedia.org/wikipedia/commons/4/4f/Laravel_logo.png')
+                    ->url('http://laravel.com')
+            );
+    }
+}
