6c70f67c68dbed02a806347a1cb98f1a5b465ddb
Jean Manzo <jmanzosistemas@gmail.com>
2017-08-04 23:56:01 -0400

    [TNP-1] viewing editing and adding users in welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,8 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
     <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <!-- OneSignal Scripts -->
-        <link rel="manifest" href="https://ab1ee076.ngrok.io/miniapp/public/manifest.json">
+        <link rel="manifest" href="https://5cf5c268.ngrok.io/manifest.json">
         <script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async></script>
         <script>
             var OneSignal = window.OneSignal || [];
@@ -15,10 +11,10 @@
                 appId: "e1cc513e-6a24-4936-b260-5741ade6f266",
                 autoRegister: false,
                 notifyButton: {
-                    enable: true, /* Set to false to hide */
-                    size: 'medium', /* One of 'small', 'medium', or 'large' */
-                    theme: 'default', /* One of 'default' (red-white) or 'inverse" (white-red) */
-                    position: 'bottom-right', /* Either 'bottom-left' or 'bottom-right' */
+                    enable: true, 
+                    size: 'medium',
+                    theme: 'default',
+                    position: 'bottom-right',
                     text: {
                         'tip.state.unsubscribed': 'Subscribe to notifications',
                         'tip.state.subscribed': "You're subscribed to notifications",
@@ -36,6 +32,9 @@
                 }
             }]);
         </script>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
 
